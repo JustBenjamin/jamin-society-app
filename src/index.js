778aec62bb660b0af946d0ffc05a836f4ad76ab8
Benjamin Hove <benhove86@gmail.com>
2022-12-29 16:17:58 -0500

    Production Version 1.1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Layout from "./pages/layout";
+import Home from './pages/home';
+import Login from './pages/login';
 
-const myFirstElement = <h1>Jamin Society</h1>
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+          <Route path="/"  element={<Layout />}/>
+          <Route index  element={<Home />}/>
+          <Route path="login"  element={<Login />}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(myFirstElement);
+root.render(<App />);
